312e686cdec9d00ffa89ef9d80a9454674453fc9
Prema <premlathag@yahoo.com>
2021-01-27 15:08:55 -0500

    first comit

---
+++ src/test/java/qaclickacademy/Mavenjava/RESTAPITest.java
@@ -0,0 +1,18 @@
+package qaclickacademy.Mavenjava;
+
+import org.testng.annotations.Test;
+
+public class RESTAPITest {
+	
+	@Test
+	public void PostJira() {
+		
+		System.out.println("PostJira");
+	}
+
+	@Test
+	public void deltweeter() {
+		
+		System.out.println("delTweeter");
+	}
+}
---
+++ src/test/java/qaclickacademy/Mavenjava/SeleniumTest.java
@@ -0,0 +1,17 @@
+package qaclickacademy.Mavenjava;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+	
+	//First execution is how to execute all testcases from Test folder-mvn test from
+	//command prompt
+	
+	//second testing, testeing XMLfile fromMaven
+
+	@Test
+	public void BrowserAutomation() {
+	
+	System.out.println("SeleniumTest");
+}
+}
